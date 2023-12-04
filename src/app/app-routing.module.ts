b51877654eb55f20ab7ca3dd9da1f40c87a212db
Likhitha4131 <likhitha4131@gmail.com>
2023-12-04 22:19:54 +0530

    CSS changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
+// app-routing.module.ts
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { CandidateListComponent } from './candidate-list/candidate-list.component';
+import { JobListComponent } from './job-list/job-list.component';
+import { ApplicationFormComponent } from './application-form/application-form.component';
+import { AddJobFormComponent } from './add-job-form/add-job-form.component';
+const routes: Routes = [
+  { path: 'candidates', component: CandidateListComponent }, 
+  { path: 'jobs', component: JobListComponent },
+  {path:'addjob',component:AddJobFormComponent},
+  { path: 'apply', component:ApplicationFormComponent},
+  { path: '', redirectTo: '/jobs', pathMatch: 'full' },
 
-
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
